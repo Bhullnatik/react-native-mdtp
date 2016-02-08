02b7cf3eccc0fdfdfdf3c3c35e4a2f73fb93d798
Jimmy Mayoukou <Bhullnatik@gmail.com>
2016-02-08 18:53:17 +0100

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,7 @@
 module.exports = {
   showDatePicker() {
     console.warn('React-Native Material DateTimePicker is not implemented on iOS.');
-  }
-
+  },
   showTimePicker() {
     console.warn('React-Native Material DateTimePicker is not implemented on iOS.');
   }
